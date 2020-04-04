332b674533c7e15c870cdf01263ecb59238b1e3f
Nadia <atiyeh.nadiaa@gmail.com>
2020-04-03 22:41:44 -0600

    finshed methods and testcases

--- src/AminoAcidLL.java
+++ src/AminoAcidLL.java
@@ -1,10 +1,10 @@
 import java.util.Arrays;
 
 class AminoAcidLL {
-    private char aminoAcid;
-    private String[] codons;
-    private int[] counts;
-    private AminoAcidLL next;
+    char aminoAcid;
+    String[] codons;
+    int[] counts;
+    AminoAcidLL next;
 
 
     AminoAcidLL() {
@@ -31,13 +31,16 @@ class AminoAcidLL {
      * If there is no next node, add a new node to the list that would contain the codon.
      */
     private void addCodon(String inCodon) {
+        //base case
         if (aminoAcid == AminoAcidResources.getAminoAcidFromCodon(inCodon)){
             incrementCodons(inCodon);
         }
         else {
             if (next != null) {
+                //recursive case
                 next.addCodon(inCodon);
             } else {
+                // adds codon to list
                 AminoAcidLL aminoacid = new AminoAcidLL(inCodon);
                 next = aminoacid;
             }
@@ -57,12 +60,14 @@ class AminoAcidLL {
     /********************************************************************************************/
     /* Shortcut to find the total number of instances of this amino acid */
     private int totalCount() {
+        //adds all the counts in the counts array
         int sum = 0;
         for (int i = 0; i < counts.length; i++) {
             sum += counts[i];
         }
         return sum;
     }
+
 
     /********************************************************************************************/
     /* helper method for finding the list difference on two matching nodes
@@ -86,52 +91,89 @@ class AminoAcidLL {
     /* Recursive method that finds the differences in **Amino Acid** counts.
      * the list *must* be sorted to use this method */
     public int aminoAcidCompare(AminoAcidLL inList) {
-        inList = sort(inList);
-        if(next == null & inList.next == null){
-            return codonDiff(inList);
+        //Checks if Linked List is sorted
+        if (inList.isSorted() == false) {
+            //if not sorted, sorts the list
+            inList = sort(inList);
         }
-        if(next == null){
+        // base case
+        // if both lists are at their end takes the difference of counts of each Amino acid
+        if (next == null && inList.next == null) {
+            return totalDiff(inList);
+        }
+
+        //if the first list reaches its end, while the other one does not
+        // takes the total count of inList and adds/recursively calls codonCompare
+        if (next == null) {
             return inList.totalCount() + aminoAcidCompare(inList);
         }
-        if(inList.next == null){
+        // if inList reaches its end
+        // takes the total count of the original list
+        // and adds it to the comparison(int) between the original list and inList
+        if (inList.next == null) {
             return totalCount() + next.aminoAcidCompare(inList);
         }
+        // if neither end reaches its end adds the totalDif of counts with
+        // the comparison between the original list and the next position in inList
+        // (iterates through the rest if the list)
         return totalDiff(inList) + next.aminoAcidCompare(inList.next);
+
+
     }
 
     /********************************************************************************************/
     /* Same ad above, but counts the codon usage differences
      * Must be sorted. */
     public int codonCompare(AminoAcidLL inList) {
-        inList = sort(inList);
-        if(next == null & inList.next == null){
-            return codonDiff(inList);
+        // Checks if Linked List is sorted
+        if (!inList.isSorted()) {
+            //if not sorted, sorts the list
+            inList = sort(inList);
         }
-        if(next == null){
+        //base case
+        // if both lists are in the same length and takes the difference of the total number of counts
+        if (inList.next == null && next == null) {
+            return totalDiff(inList);
+        }
+        // if first list reaches its
+        if (next == null) {
             return inList.totalCount() + codonCompare(inList);
         }
-        if(inList.next == null){
+        if (inList.next == null) {
             return totalCount() + next.codonCompare(inList);
         }
-        return totalDiff(inList) + next.codonCompare(inList.next);
+        if(aminoAcid < inList.aminoAcid){
+            return totalCount() + next.codonCompare(inList);
+        }
+        if(aminoAcid > inList.aminoAcid){
+            return inList.totalCount() + codonCompare(inList.next);
+        }
+        return codonDiff(inList) + next.codonCompare(inList.next);
     }
 
 
     /********************************************************************************************/
     /* Recursively returns the total list of amino acids in the order that they are in in the linked list. */
     public char[] aminoAcidList() {
+        // if list is empty or points to null returns default aminoAcid
         if (next == null) {
             return new char[]{aminoAcid};
         }
         else{
+            //initializes a char [] that recursively calls the next position of the list (iterates through list)
+            // * does not start at the start of the list*
             char[] current = next.aminoAcidList();
+            // creates empty char [] that will store the Amino Acids, uses the length of the list of the current
+            // * adds one to the length, since current does not begin at the start of the list*
             char[] aminoAcidList = new char[current.length + 1];
-
+            // iterates through the aminoAcidList and then assigns each index to the index of the char [] current
             for (int i = 0; i < current.length; i++) {
+                //assigns the first position of the amino acid [] to the first amino acid in the list
                 aminoAcidList[0] = aminoAcid;
+                // does this for the rest of the positions not including 0
                 aminoAcidList[i + 1] = current[i];
             }
-            printArray(aminoAcidList);
+            //printArray(aminoAcidList);
             return aminoAcidList;
         }
     }
@@ -151,13 +193,15 @@ class AminoAcidLL {
             return new int []{totalCount()};
         }
         else{
+            //does the same as aminoAcidList
+            // calls aminoAcid counts recursively
             int[] current = next.aminoAcidCounts();
             int[] aminoAcidCounts = new int[current.length + 1];
             for (int i = 0; i < current.length; i++) {
                 aminoAcidCounts[0] = totalCount();
                 aminoAcidCounts[i + 1] = current[i];
             }
-            printArray(aminoAcidCounts);
+            //printArray(aminoAcidCounts);
             return aminoAcidCounts;
         }
     }
@@ -172,12 +216,15 @@ class AminoAcidLL {
     /********************************************************************************************/
     /* recursively determines if a linked list is sorted or not */
     public boolean isSorted() {
+        //base case
         if (next == null) {
             return true;
         }
+        //checks if the ASCII value of the first element of the list is greater than the next element of the list
         if (aminoAcid > next.aminoAcid) {
             return false;
         }
+        //recursive call
         return next.isSorted();
     }
 
@@ -186,12 +233,25 @@ class AminoAcidLL {
     /* Static method for generating a linked list from an RNA sequence */
 
     public static AminoAcidLL createFromRNASequence(String inSequence) {
+        // creates new Linked List with the first three characters of inSequence
         AminoAcidLL list = new AminoAcidLL(inSequence.substring(0, 3));
+        //updates the string inSequence to include the rest of the string excluding the first three chars
         inSequence = inSequence.substring(3);
+        //uses for loop to iterate through the string and take substrings of every three letter sequence
         for (int i = 0; i < inSequence.length(); i++) {
-            list.addCodon(inSequence.substring(0, 3));
-            inSequence = inSequence.substring(3);
-            i--;
+            // checks if the sequence does not have a stop, and if it does breaks out of the loop
+            if(AminoAcidResources.getAminoAcidFromCodon(inSequence.substring(0,3)) == '*'){
+                break;
+            }
+            else{
+                // adds the Amino Acid for ever three letter sequence
+                list.addCodon(inSequence.substring(0, 3));
+                //updates inSequence same as above
+                inSequence = inSequence.substring(3);
+                i--;
+            }
+            //decreases the iterator to avoid out-of-bounds exceptions, and to avoid missing the last codon of the string
+
         }
         printList(list);
         return list;
@@ -210,13 +270,14 @@ class AminoAcidLL {
                 for (AminoAcidLL j = i.next; j != null; j = j.next) {
                     // uses selection sort to swap
                     if (i.aminoAcid > j.aminoAcid) {
+                        //swap takes place
                         char temp = i.aminoAcid;
                         i.aminoAcid = j.aminoAcid;
                         j.aminoAcid = temp;
                     }
                 }
             }
-            printList(inList);
+            //printList(inList);
             return inList;
         }
     }
@@ -226,7 +287,7 @@ class AminoAcidLL {
         while (head != null) {
             System.out.print(head.aminoAcid + " ");
             //System.out.print(Arrays.toString(head.codons)+ " ");
-            //System.out.print(Arrays.toString(head.counts)+ " ");
+            System.out.print(Arrays.toString(head.counts)+ " ");
 
             head = head.next;
         }
--- src/AminoAcidLLTester.java
+++ src/AminoAcidLLTester.java
@@ -4,44 +4,200 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class AminoAcidLLTester {
+
+    /* Test's the basic functionality comparing counts of Amino Acids
+     * Creates a linked list and then takes the total amount of AminoAcids
+     * (includes repeating), and will take the difference of the two
+     * Input: GGGGCCGUC --> List 1
+     * Input: UACGCC --> List 2
+     * List 1 --> A A V (Count: 3)
+     * List 2 --> T A (Count: 2)
+     * 3 - 2 = 1
+     * Expected: 1
+     */
+
     @Test
-    public void testCreateFromRNASequence() {
-        String a = "GCU";
+    public void testAminoAcidCompare1() {
+        String a = "GGGGCCGUC";
         AminoAcidLL list = new AminoAcidLL();
-        list.createFromRNASequence(a);
+        list = list.createFromRNASequence(a);
+        String b = "UACGCC";
+        AminoAcidLL list2 = new AminoAcidLL();
+        list2 = list2.createFromRNASequence(b);
+        list.codonCompare(list2);
+        assertEquals(list.aminoAcidCompare(list2), 1);
 
     }
+
+    /* Test's the basic functionality comparing counts of Amino Acids
+     * Creates a linked list and then takes the total amount of AminoAcids
+     * (includes repeating), and will take the difference of the two
+     * Input: AAGAAAACCACAUCU --> List 1
+     * Input: AAACGGUGG --> List 2
+     * List 1 --> K K T T S (Count: 5)
+     * List 2 --> K R W (Count: 3)
+     * 5 - 3 = 2
+     * Expected: 2
+     */
+
+    @Test
+    public void testAminoAcidCompare2(){
+        String a = "AAGAAAACCACAUCU";
+        String b = "AAACGGUGG";
+        AminoAcidLL list1 = new AminoAcidLL();
+        AminoAcidLL list2 = new AminoAcidLL();
+        list1 = list1.createFromRNASequence(a);
+        list2 = list2.createFromRNASequence(b);
+        assertEquals(list1.aminoAcidCompare(list2),2 );
+    }
+
+    /* Test's the basic functionality comparing counts of Amino Acids
+     * Creates a linked list and then takes the total amount of AminoAcids
+     * (includes repeating), and will take the difference of the two
+     * Input: AAGAAAACCACAUCU --> List 1
+     * Input: AAACGGUGG --> List 2
+     * List 1 --> K             T               S
+     *           [1,1] [0, 1, 1, 0, 0, 0] [0, 0, 0, 0, 0, 1]
+     * List 2 --> K           R            W
+     *           [0,1] [0, 0, 1, 0, 0, 0] [1]
+     * K: 2 - 1 = 1; T: 2 - 1 = 1; S: 1; W: 1;
+     * CodonDiff = 1 + 1 + 1 + 1 = 4
+     * Expected: 4
+     */
+
+    @Test
+    public void testCodonCompare1(){
+        String a = "AAGAAAACCACAUCU";
+        String b = "AAACGGUGG";
+        AminoAcidLL list1 = new AminoAcidLL();
+        AminoAcidLL list2 = new AminoAcidLL();
+        list1 = list1.createFromRNASequence(a);
+        list2 = list2.createFromRNASequence(b);
+        assertEquals(list1.codonCompare(list2), 4);
+    }
+
+    /* Test's the basic functionality comparing counts of Amino Acids
+     * Creates a linked list and then takes the total amount of AminoAcids
+     * (includes repeating), and will take the difference of the two
+     * Input: AAGAAAACCACAUCU --> List 1
+     * Input: GGUGGAGCUGGU --> List 2
+     * List 1 --> E         G              A       K
+     *           [1,1] [1, 0, 0, 0] [0, 0, 1, 0] [1,0]
+     * List 2 --> G1            A1          G2          A2
+     *           [0,1,0,2] [0, 0, 0, 1] [0,1,0,2] [0, 0, 0, 1]
+     * E: 2; G1: 1 - 3 = 2; A2: 1- 1 = 0; K:1; G2: 3 - 1 = 2; A2: 1- 1 =0;
+     * CodonDiff = 2 + 2 + 0 + 1 + 2 = 7
+     * Expected: 7
+     */
+
+    @Test
+    public void testCodonCompare2(){
+        String a = "GAGGAAGGGGCCAAG";
+        String b = "GGUGGAGCUGGU";
+        AminoAcidLL list1 = new AminoAcidLL();
+        AminoAcidLL list2 = new AminoAcidLL();
+        list1 = list1.createFromRNASequence(a);
+        list2 = list2.createFromRNASequence(b);
+        System.out.println(list1.aminoAcidCompare(list2));
+        assertEquals(list1.codonCompare(list2), 7);
+    }
+
+    /* Test's the basic functionality on how to create
+    * a Linked List from a RNA sequence
+    * Input: GCUCGGCCGAAGGGAAGC
+    * Expected: A R P K G S
+    * Assert Equals Expected : A
+    *   assertEquals will only test if the first node
+    *   matches the amino acid from expected. */
+
+    @Test
+    public void testCreateFromRNASequence1() {
+        String a = "GCUCGGCCGAAGGGAAGC";
+        AminoAcidLL list = new AminoAcidLL();
+        list.createFromRNASequence(a);
+        assertEquals(list.createFromRNASequence(a).aminoAcid, 'A');
+    }
+
+    /* Test's the basic functionality on how to create
+     * a Linked List from a RNA sequence, testing with other
+     * factors like repeating codons and a stop
+     * Input: GCUGCUACGGAGCUUCGGAGCCCGUAGGUGGACAAG
+     * Expected: A T E L R S P
+     * Assert Equals Expected : A
+     *   assertEquals will only test if the first node
+     *   matches the amino acid from expected. */
+
+    @Test
+    public void testCreateFromRNASequence2() {
+        String a = "GCUGCUACGGAGCUUCGGAGCCCGUAGGUGGACAAG";
+        AminoAcidLL list = new AminoAcidLL();
+        assertEquals(list.createFromRNASequence(a).aminoAcid, 'A');
+    }
+
+    /* Test's the basic functionality on how to check if the linked list is sorted
+     * Creates a linked list first, and then test the isSorted method
+     * Checks it with other edge cases like STOP, and repeating aminoacids
+     * Input: GCUACGGAGCUUCGGAGCCCGUAGGUGGACAAG
+     *  List A T E L R S P (Not Sorted)
+     * Expected: False
+     */
 
     @Test
     public void testisSorted() {
         String a = "GCUACGGAGCUUCGGAGCCCGUAGGUGGACAAG";
         AminoAcidLL list = new AminoAcidLL();
         list = list.createFromRNASequence(a);
-        list.isSorted();
+        assertEquals(list.isSorted(), false);
     }
+
+    /* Test's the basic functionality on generating a char [] of the amino acids from the linked listt
+     * Will first generate a linked list, and then implement the aminoAcidList method
+     * Checks it with other edge cases like repeating amino acids
+     * Input: GCUACGGAGCUUCGGAGCCCGUAGGUGGACAAGGCUGCUGCU
+     * Expected: A T E L R S P V D K
+     */
 
     @Test
     public void testAminoAcidList() {
-        String a = "GCUACGGAGCUUCGGAGCCCGUAGGUGGACAAGGCUGCUGCU";
+        String a = "GCUACGGAGCUUCGGAGCCCGGUGGACAAGGCUGCUGCUGCUGCUACG";
         AminoAcidLL list = new AminoAcidLL();
         list = list.createFromRNASequence(a);
-        list.aminoAcidList();
+        char [] answer = {'A', 'T', 'E', 'L', 'R', 'S', 'P', 'V', 'D', 'K'};
+        assertArrayEquals(list.aminoAcidList(), answer);
     }
+
+    /* Test's the basic functionality on generating a int [] of how many times the aminoacid appears in the linked listt
+     * Will first generate a linked list, and then implement the aminoAcidList method
+     * Checks it with other edge cases like repeating amino acids
+     * Input: GCUACGGAGCUUCGGAGCCCGGUGGACAAGGCUGCUGCUGCUGCUACG
+     * Expected: 6 2 1 1 1 1 1 1 1 1
+     */
 
     @Test
     public void testAminoAcidCounts() {
-        String a = "GCUACGGAGCUUCGGAGCCCGUAGGUGGACAAGGCUGCUGCUGCUGCUACG";
+        String a = "GCUACGGAGCUUCGGAGCCCGGUGGACAAGGCUGCUGCUGCUGCUACG";
         AminoAcidLL list = new AminoAcidLL();
         list = list.createFromRNASequence(a);
-        list.aminoAcidCounts();
+        int [] answer = {6,2,1,1,1,1,1,1,1,1};
+        assertArrayEquals(list.aminoAcidCounts(), answer);
     }
+
+    /* Test's the basic functionality of sorting a linked list
+     * Will first generate a linked list, and then implement the sort method
+     * Input: GCUACGGAGCUUCGGAGCCCGUAGGUGGACAAG
+     *  List: T E L R S P
+     * Output: E L P R S T
+     * Assert Equals Expected : E
+     *   assertEquals will only test if the first node
+     *   matches the amino acid from expected.
+     * */
 
     @Test
     public void testSort() {
-        String a = "GCUACGGAGCUUCGGAGCCCGUAGGUGGACAAG";
+        String a = "ACGGAGCUUCGGAGCCCGUAGGUGGACAAG";
         AminoAcidLL list = new AminoAcidLL();
         list = list.createFromRNASequence(a);
-        list.sort(list);
+        assertEquals(list.sort(list).aminoAcid, 'E');
     }
 }
 
